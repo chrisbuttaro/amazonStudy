0203577080c906392251259d3b9e312ee40d8cb2
Chris Buttaro <chris.buttaro@gmail.com>
2020-11-15 11:53:37 -0800

    Update twoSum.java
    Added Java Doc.

--- twoSum.java
+++ twoSum.java
@@ -1,5 +1,12 @@
 class Solution {
     
+     /**
+     * Given an array of integers nums and an integer target, returns indices of the two numbers such that they add up to target.
+     * Assumes that each input would have exactly one solution, and the same element is not used twice.
+     * Returns with index[0] < index[1].
+     *
+     * Constraints: 2 <= nums.length <= 10^5,  -10^9 <= nums[i] <= 10^9,   -10^9 <= target <= 10^9
+     */
      public int[] twoSum(int[] nums, int target) {
         int [] retVal = new int[2];
         HashSet<Integer> numsHashSet = new HashSet<>();
