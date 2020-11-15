961e81e2faecb3ced042367e8f066afb69413f33
Chris Buttaro <chris.buttaro@gmail.com>
2020-11-15 11:31:28 -0800

    Update twoSum.java
    Break the outer loop when retVal[1] is assigned.

--- twoSum.java
+++ twoSum.java
@@ -1,16 +1,13 @@
-//https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/508/
-
 class Solution {
     
      public int[] twoSum(int[] nums, int target) {
-         
         int [] retVal = new int[2];
         HashSet<Integer> numsHashSet = new HashSet<>();
          
         for (int i=0; i<nums.length; i++) {
             numsHashSet.add(nums[i]);
-            
-            Integer otherValue = target - nums[i];
+            Integer otherValue = target - nums[i]; 
+            // if nums[i] + otherValue equals target we've found the pair. 
             if(numsHashSet.contains(otherValue)) {
                 for (int j=0; j<nums.length; j++) {
                     if (nums[j] == otherValue && j != i) {
@@ -20,8 +17,9 @@ class Solution {
                     }
                 }
             }
+            if (retVal[1] !=0)  // index 1 will not be 0 if assigned.
+                break;
         } 
-
         return retVal;
     }
 
